445f66688bbcc8e3e4a311c879376ff5f05238a0
subhash1236 <subhashsingh1236gmail,com>
2023-12-22 22:12:25 +0530

    first commit

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,70 +1,160 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { useContext, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/images/logo/logo.png";
 
+import { NavDropdown } from "react-bootstrap";
+import { AuthContext } from "../context/AuthProvider";
+
 const NavItems = () => {
-    const [menuToggle, setMenuToggle] = useState(false);
-    const [socialToggle, setSocialToggle] = useState(false);
-    const [headerFixed, setHeaderFixed] = useState(false);
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [socialToggle, setSocialToggle] = useState(false);
+  const [headerFiexd, setHeaderFiexd] = useState(false);
 
-    //addeventListener
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 200) {
-            setHeaderFixed(true);
-        } else {
-            setHeaderFixed(false);
-        }
-    })
-    return (
-        <header className={`header-section style-4 ${headerFixed ? "header-fixed fadeInUp" : ""}`}>
-            {/*header top start */}
-            <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
-                <div className="container">
-                    <div className="header-top-area">
-                        <Link to="/signup" className='lab-btn me-3'><span>Create Account</span></Link>
-                        <Link to="/login">log in</Link>
-                    </div>
-                </div>
+  // check if user is registered
+  const { user, logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    console.log("Attempting to log out...");
+    logout()
+      .then(() => {
+        console.log("Logout successful.");
+      })
+      .catch((error) => {
+        console.error("Logout error:", error);
+      });
+  };
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+      setHeaderFiexd(true);
+    } else {
+      setHeaderFiexd(false);
+    }
+  });
+
+  return (
+    <header
+      className={`header-section style-4 ${
+        headerFiexd ? "header-fixed fadeInUp" : ""
+      }`}
+    >
+      {/* ------ header top: first div ----- */}
+      <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
+        <div className="container">
+          <div className="header-top-area">
+            <Link to="/signup" className="lab-btn me-3">
+              <span>Create Account</span>
+            </Link>
+            <Link to="/login">Log In</Link>
+          </div>
+        </div>
+      </div>
+
+      {/* header top ends*/}
+
+      {/* ---header botton starts */}
+      <div className="header-bottom">
+        <div className="container">
+          <div className="header-wrapper">
+            {/* logo  */}
+            <div className="logo-search-acte">
+              <div className="logo">
+                <Link to="/">
+                  <img src={logo} alt="logo" />
+                </Link>
+              </div>
             </div>
-            <div className='header-bottom'>
-                <div className="container">
-                    <div className="header-wrapper">
-                        <div className='logo-search-acte'>
-                            <div className="logo">
-                                <Link to={"/"}>
-                                    <img src={logo} alt="" />
-                                </Link>
-                            </div>
 
-                        </div>
-                        <div className="menu-area">
-                            <div className="menu">
-                                <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
-                                    <li><Link to="/">Home</Link></li>
-                                    <li><Link to="/shop">Shop</Link></li>
-                                    <li><Link to="/blog">Blog</Link></li>
-                                    <li><Link to="/about">About</Link></li>
-                                    <li><Link to="/contact">Contact</Link></li>
-                                </ul>
+            {/* menu area */}
+            <div className="menu-area">
+              <div className="menu">
+                <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="shop">Shop</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                  <li>
+                    {" "}
+                    <NavLink to="/about">About</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </li>
+                </ul>
+              </div>
 
-                            </div>
-                            <Link to="/sign-up" className='lab-btn me-3 d-none d-md-block'>Create-Account</Link>
-                            <Link to="/login" className='d-none d-md-block'>Log in</Link>
-                            <div onClick={() => setMenuToggle(!menuToggle)} className={`header-bar d-lg-none ${menuToggle ? "active" : ""}`}>
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                            <div className='ellepsis-bar d-md-none ' onClick={()=>setSocialToggle(! socialToggle)}>
-                            <i className="icofont-info-square"></i>
-                            </div>
+              {/* users when user is available */}
+              {user ? (
+                <>
+                  <div>
+                    {user?.photoURL ? (
+                      <>
+                        <img src={user?.photoURL} className="nav-profile" />
+                      </>
+                    ) : (
+                      <img
+                        src="/src/assets/images/author/01.jpg"
+                        className="nav-profile"
+                      />
+                    )}
+                  </div>
+                  <NavDropdown id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1" onClick={handleLogout}>
+                      Logout
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/cart-page">
+                      Shopping Cart
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/cart-page">Order</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <Link
+                    to="/sign-up"
+                    className="lab-btn me-3 d-none d-md-block"
+                  >
+                    <span>Create Account</span>
+                  </Link>
+                  <Link to="/login" className="d-none d-md-block">
+                    Log In
+                  </Link>
+                </>
+              )}
 
-                        </div>
-                    </div>
-                </div>
+              {/* menu toggle btn */}
+              <div
+                className={`header-bar d-lg-none ${menuToggle ? "active" : ""}`}
+                onClick={() => setMenuToggle(!menuToggle)}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+
+              {/* social toggler */}
+              <div
+                className="ellepsis-bar d-md-none"
+                onClick={() => setSocialToggle(!socialToggle)}
+              >
+                <i className="icofont-info-square"></i>
+              </div>
             </div>
-        </header>
-    )
-}
+          </div>
+        </div>
+      </div>
+      {/* header bottom ends */}
+    </header>
+  );
+};
 
-export default NavItems
+export default NavItems;
